87a1f568990afa0f989ca146889366d28ba86c01
Matt Pike <mattp91@gmail.com>
2025-09-04 22:38:05 -0400

    Scaffold frontend with Vite and initial auth pages.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to AlyadTrading</h1>
+      <p>Track and research your options trading performance.</p>
+      <a href="/signup">Sign Up</a> | <a href="/login">Login</a>
+    </div>
+  );
+};
+
+export default Home;
